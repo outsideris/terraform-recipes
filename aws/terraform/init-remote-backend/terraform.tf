7c00593a32797dfc70976d3947d3de6d0d2736b9
Outsider <outsideris@gmail.com>
2018-08-10 23:44:56 +0900

    aws: fix wrong names
    Signed-off-by: Outsider <outsideris@gmail.com>

--- aws/terraform/init-remote-backend/terraform.tf
+++ aws/terraform/init-remote-backend/terraform.tf
@@ -2,11 +2,11 @@ terraform {
   required_version = ">= 0.11.7"
 
   backend "s3" {
-    bucket         = "test.kr.sideeffect.terraform.state"
+    bucket         = "your.bucket.name.for.tfstate"
     key            = "path/to/terraform.tfstate"
-    region         = "ap-northeast-1"                     // e.g. ap-northeast-1
+    region         = "YOUR_REGION"                     // e.g. ap-northeast-1
     encrypt        = true
-    dynamodb_table = "OutsiderTestTerraformStateLock"
+    dynamodb_table = "YOUR_DYNAMODB_LOCK_TABLE_NAME"
     acl            = "bucket-owner-full-control"
   }
 }
